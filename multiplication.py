3b13833434a602e6dc6fabc29f03bbfe204a0173
pandeybivek101 <48467101+pandeybivek101@users.noreply.github.com>
2023-01-31 21:15:55 +0200

    Update multiplication.py
    documenting the matrix code

--- multiplication.py
+++ multiplication.py
@@ -1,19 +1,31 @@
 import numpy as np
 
-
+"""
+generating the random matries of specified length 
+for a, b and c using numpy
+"""
 a = np.random.randint(10, size=(10**6, 10**3))
 b = np.random.randint(10, size=(10**3, 10**6))
 c = np.random.randint(10, size=(10**6, 1))
 
 
+"""
+The function to handle the multiplication of two matrices
+here @ is numpy operator to multiply matrices
+"""
 
 def do_multiplication(matrix_1, matrix_2):
      return matrix_1 @ matrix_2
     
 
 def handle_multipliction(matrix_1, matrix_2, matrix_3):
+
+    #first two matrices is multiplied
     firt_multiplication = do_multiplication(matrix_1, matrix_2)
+
+    #the resuting matrix is multiplied with third matrix
     second_multiplication = do_multiplication(firt_multiplication, matrix_3)
+    
     return second_multiplication
 
 
